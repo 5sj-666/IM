29f90cb03ec9928a2b7bd26b7ed7bb6d5e461e70
fffuture <562258693@qq.com>
2021-04-10 00:33:13 +0800

    feat: try use notification

--- IM/src/main.ts
+++ IM/src/main.ts
@@ -5,6 +5,22 @@ import { createRouter, createWebHistory } from "vue-router";
 import routes from "./router";
 import './registerServiceWorker'
 
+// import image form "@/assets/logo.png"
+
+Notification.requestPermission(function(result) {
+    if (result === 'granted') {
+      navigator.serviceWorker.ready.then(function(registration) {
+        let message = {
+          badge: '',
+          body: "测试消息内容",
+          // image: 
+        };
+        // registration.showNotification('Notification with ServiceWorker');
+        registration.showNotification('title', message);
+      });
+    }
+});
+
 const router = createRouter({history: createWebHistory(), routes: routes});
 
 createApp(App)
