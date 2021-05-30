4ece19b4df40270ae0c18f7e552e92b2eaa015f3
fffuture <562258693@qq.com>
2021-05-31 00:24:57 +0800

    optimize: 好友列表sql; feat: 新增websocket连接

--- IM-koa/src/index.js
+++ IM-koa/src/index.js
@@ -1,23 +1,26 @@
 const Koa = require('koa');
 const app = new Koa();
 const KoaRouter = require('koa-router');
+const KoaStatic = require('koa-static');
 const router = KoaRouter();
 // const { hasUser, addUser } = require('./model/user.js');
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 var bodyParser = require('koa-bodyparser');
+// var path = require('path');
+const WebSocket = require('ws');
+const http = require('http');
+// const wsTool = require();
+// import wsTool from './utils/wsTool'
+const wsTool = require('./utils/wsTool');
 
 const { userCtrol } =  require("./control/userCtrol.js");
-
 const { friendCtrol } =  require("./control/friendCtrol.js");
 
-
+app.use(KoaStatic(`${__dirname}/asset/`) );
 app.use(bodyParser());
 
-
 app.use(router.routes())//启动路由
-
 app.use(userCtrol.routes());
-
 app.use(friendCtrol.routes());
 
 app.use(async (ctx) => {
@@ -36,46 +39,17 @@ router.get('/testRTC',(ctx)=>{
 });
 
 
-app.listen(3000);
 
 
 
+const server = http.createServer(app.callback());//koa脚手架创建的服务器
+let ws = new WebSocket.Server({
+    server: server//挂载到原有服务器上，不然你得重新开一个端口，详见文档
+})
+wsTool(ws);
 
 
-// // router.get('/api/user/hasUser',async ctx => {
-// router.post('/api/user/hasUser',async ctx => {
-//   // console.log("req ctx", ctx);
-//   let data = ctx.request.body;
-//   console.log("---req ctx data ", data);
-//   let result = await hasUser(data.account, data.password);
-//   console.log("result:", result);
-//   if(result) {
-//     let payload = {userNumber:"admin5", time:new Date().getTime(), timeout:1000*60*60*2}
-//     let token = jwt.sign(payload, 'wsj');
-//     console.log("---token: ", token);
-//     ctx.body = JSON.stringify({ok: true, status: 200, msg:"登录成功", token});
-//   }else {
-//     ctx.body = JSON.stringify({ok: false, status: 200, error: '账号或密码错误'});
-//   }
-              
-
-//   // jwt.sign(JSON.stringify, secret);
-  
-//   // let verifyCode = jwt.verify(token, 'wsj');
-//   // console.log("---verify code: ", verifyCode)
-
-//   // ctx.response.status= 200;
-//   ctx.set('Content-Type', 'application/json');
-//   ctx.set('access-control-allow-origin', '*');
-//   // console.log("res ctx", ctx);
-// });
-
-// router.post('/api/user/register', async ctx=>{
-//   let data = ctx.request.body;
-//   console.log("---req add ctx data ", data);
-//   let result = await addUser(data.account, data.password);
-//   console.log("---add user result:", result);
-//   ctx.set('Content-Type', 'application/json');
-//   ctx.set('access-control-allow-origin', '*');
-//   ctx.body=JSON.stringify(result);
-// })
+// app.listen(3000);
+server.listen(3000, (err) => { 
+  !!err ? console.log('服务启动出错', err) : console.log('IM运行在http3000端口');
+});
